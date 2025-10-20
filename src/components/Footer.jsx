ea852ffceaf8b02e573186af91726ae4386697f1
TanujaPammina <tanujapammina@gmail.com>
2025-10-20 20:42:46 +0530

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-dark text-white text-center py-3 mt-auto">
-      <p>© 2025 QuizMania. Built with ❤️ by CSE-D,ANITS!</p>
+      <p>© 2025 QuizMania. Built with ❤️ by CSM-C, Tanuja Pammina, ANITS!</p>
     </footer>
   );
 };
